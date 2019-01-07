fda826371f85c19168646fad1318eed09190d297
Kim Shepherd <kim.shepherd@vibrato.com.au>
2019-01-07 16:10:13 +1100

    Added the rest of the definition properties except management group

--- azurerm/data_source_policy_definition.go
+++ azurerm/data_source_policy_definition.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/structure"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/helpers/validate"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/utils"
 )
@@ -40,7 +41,7 @@ func dataSourceArmPolicyDefinition() *schema.Resource {
 
 			"description": {
 				Type:     schema.TypeString,
-				Optional: true,
+				Computed: true,
 			},
 
 			"policy_rule": {
@@ -76,6 +77,43 @@ func dataSourceArmPolicyDefinitionRead(d *schema.ResourceData, meta interface{})
 	}
 
 	d.SetId(*resp.ID)
+
+	if props := resp.DefinitionProperties; props != nil {
+		d.Set("policy_type", props.PolicyType)
+		d.Set("mode", props.Mode)
+		d.Set("display_name", props.DisplayName)
+		d.Set("description", props.Description)
+
+		if policyRule := props.PolicyRule; policyRule != nil {
+			policyRuleVal := policyRule.(map[string]interface{})
+			policyRuleStr, err := structure.FlattenJsonToString(policyRuleVal)
+			if err != nil {
+				return fmt.Errorf("unable to flatten JSON for `policy_rule`: %s", err)
+			}
+
+			d.Set("policy_rule", policyRuleStr)
+		}
+
+		if metadata := props.Metadata; metadata != nil {
+			metadataVal := metadata.(map[string]interface{})
+			metadataStr, err := structure.FlattenJsonToString(metadataVal)
+			if err != nil {
+				return fmt.Errorf("unable to flatten JSON for `metadata`: %s", err)
+			}
+
+			d.Set("metadata", metadataStr)
+		}
+
+		if parameters := props.Parameters; parameters != nil {
+			paramsVal := props.Parameters.(map[string]interface{})
+			parametersStr, err := structure.FlattenJsonToString(paramsVal)
+			if err != nil {
+				return fmt.Errorf("unable to flatten JSON for `parameters`: %s", err)
+			}
+
+			d.Set("parameters", parametersStr)
+		}
+	}
 
 	return nil
 }
